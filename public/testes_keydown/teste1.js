b9ae85a6a43f88932c6379356753897b6928a008
Gabriel Pietro <gabriel.pietro@sptech.com>
2025-05-07 22:08:57 -0300

    update: estudando mais afundo sobre keydown, realizado testes para aprimorar o sistema de combos, criação das pastas onde terão os gifs dos combos

--- public/testes_keydown/teste1.js
+++ public/testes_keydown/teste1.js
@@ -1,24 +1,37 @@
-var combo = ['d', 's', 'w', 'd'];
+var combo = [['d', 's', 'w', 'd'], ['a', 's', 'd', 'f'], ['t', 'g', 'b', 'l']];
+var comboAtual = 0
 var cont = 0;
-var acertos = 0;
+var sequencia = 0;
+var pontuacao = 0;
 
+var comboMostrado = document.getElementById('div_combo');
 var mensagem = document.getElementById('div_mensagem');
+var personagem = document.getElementById('personagem_teste');
+var pontuacaoAtual = document.getElementById('pontuacao');
 
 document.addEventListener('keydown', function (e) {
     var msg = '';
 
-    if (e.key == combo[cont]) {
+    if (e.key == combo[comboAtual][cont]) {
         msg += `Acertou a ${cont + 1}° tecla!<br>`
-        acertos++;
+        sequencia++;
         cont++;
     } else {
-        msg += `Errou a ${cont + 1}° tecla!<br>`
-        cont++;
+        msg += `Errou<br>`
+        cont = 0;
+        sequencia = 0
     }
 
     mensagem.innerHTML = msg;
 
-    if (acertos == 4) {
+    if (sequencia == 4) {
         mensagem.innerHTML += `<br> BOA IRMAO ACERTOU O COMBO!`;
+        sequencia = 0;
+        cont = 0;
+        comboAtual++;
+        pontuacao += 10;
+        pontuacaoAtual.innerHTML = pontuacao;
+        comboMostrado.innerHTML = `${combo[comboAtual]}`;
+        personagem.style.backgroundImage = `url(../../assets/gifs/combos/akuma/akuma-shouryuken-1.gif)`;
     }
 })
