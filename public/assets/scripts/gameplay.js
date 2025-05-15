7d660bcaa97c9231a39c82bfed6de174a6dabada
Gabriel Pietro <gabriel.pietro@sptech.com>
2025-05-14 21:35:07 -0300

    update: feita pagina de seleção de personagem e fundo

--- public/assets/scripts/gameplay.js
+++ public/assets/scripts/gameplay.js
@@ -1,3 +1,5 @@
+// PARTE DA SELEÇÃO DE PERSONAGENS
+
 var listCharacters = [
     ['Ryu', 'assets/gifs/ryu-afk-1.gif', 'assets/img/background-selected/ryu-background-selected.png'],
     ['Ken', 'assets/gifs/ken-afk-1.gif', 'assets/img/background-selected/ken-background-selected.png'],
@@ -6,13 +8,24 @@ var listCharacters = [
     ['Akuma', 'assets/gifs/akuma-afk-1.gif', 'assets/img/background-selected/akuma-background-selected.png']
 ];
 
-var listBackgrounds = [colocar aqui a lista dos fundos que eu achar]
+var listBackgrounds = [
+    'assets/gifs/background-gameplay/background-gameplay1.gif', 'assets/gifs/background-gameplay/background-gameplay2.gif',
+    'assets/gifs/background-gameplay/background-gameplay3.gif', 'assets/gifs/background-gameplay/background-gameplay4.gif',
+    'assets/gifs/background-gameplay/background-gameplay5.gif', 'assets/gifs/background-gameplay/background-gameplay6.gif',
+    'assets/gifs/background-gameplay/background-gameplay7.gif', 'assets/gifs/background-gameplay/background-gameplay8.gif',
+    'assets/gifs/background-gameplay/background-gameplay9.gif', 'assets/gifs/background-gameplay/background-gameplay10.gif',
+]
 
+var charScreen = document.querySelector('.select-character-background')
 var charName = document.getElementById('char_name');
 var charGif = document.getElementById('gif_selected');
 var charBackground = document.getElementById('bg_selected');
+var backgroundName = document.getElementById('bg_name');
+var background = document.querySelector('.gameplay-select-character');
+var backgroundAll = document.getElementById('background-gameplay');
 
 var current = 0;
+var currentBackground = 0;
 
 function prevCharacter() {
     if (current == 0) {
@@ -39,9 +52,56 @@ function nextCharacter() {
 }
 
 function prevBackground() {
+    if (currentBackground == 0) {
+        currentBackground = 9;
+    } else {
+        currentBackground--;
+    }
 
+    backgroundName.innerHTML = `Cenário ${currentBackground + 1}`;
+    background.style.backgroundImage = `url(${listBackgrounds[currentBackground]})`;
 }
 
 function nextBackground() {
+    if (currentBackground < listBackgrounds.length - 1) {
+        currentBackground++;
+    } else {
+        currentBackground = 0;
+    }
+
+    backgroundName.innerHTML = `Cenário ${currentBackground + 1}`;
+    background.style.backgroundImage = `url(${listBackgrounds[currentBackground]})`;
+}
+// FIM PARTE DA SELEÇÃO DE PERSONAGENS
+
+// COMEÇO GAMEPLAY
+
+var overlayTemp = document.querySelector('.overlay-temp');
+var contagemTempDiv = document.getElementById('contagem-temp');
+var contagemTemp = 3;
+
+function confirmarSelecao() {
+    setTimeout(() => {
+        charScreen.style.display = 'none';
+        backgroundAll.style.backgroundImage = `url(${listBackgrounds[currentBackground]})`;
+        overlayTemp.style.display = 'flex';
+    }, 1000)
+
+
+
+    var intervalo = setInterval(() => {
+
+        if (contagemTemp >= 0) {
+            contagemTempDiv.innerHTML = contagemTemp;
+            contagemTemp--;
+        } else {
+            contagemTempDiv.innerHTML = `HORA DE COMBAR!`;
+            clearInterval(intervalo);
+
+            setTimeout(() => {
+                overlayTemp.style.display = 'none';
+            }, 1000)
+        }
+    }, 1000);
 
 }
